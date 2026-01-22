60ac24535965f2bf406988ea0ce6cfe88bf8de6d
Kit Kennedy <kitkennedy8@gmail.com>
2026-01-21 18:30:39 -0800

    delete files from ios-app dir because moved them to xcode project dir

--- ios-app/Config.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-enum Config {
-    // Supabase Configuration
-    static let supabaseURL = "https://fhrlhhvwsexqugphpkla.supabase.co"
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZocmxoaHZ3c2V4cXVncGhwa2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3OTA2OTMsImV4cCI6MjA4NDM2NjY5M30._fwlGHw_x41tP-_mMaTLYvpIrEfhrX87Gt2IKN_ScAs"
-}
